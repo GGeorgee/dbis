5224ffb287629c58099cb54e8be59ddda81b0251
GGeorgee <37927842+GGeorgee@users.noreply.github.com>
2018-06-09 11:57:49 +0300

    Update laboratory2.sql

--- km63/Smoliak_Yurii/laboratory2.sql
+++ km63/Smoliak_Yurii/laboratory2.sql
@@ -1,2 +1,18 @@
 -- LABORATORY WORK 2
+/*1*/
+SELECT
+TRIM(Vend_Country)
+From Vendors
+Where IN (Select 
+TRIM(Order_Item)
+From ORDERITEMS)<2;
+/*2*/
+Select
+TRIM(CUST_NAME)
+From CUSTOMERS 
+WHERE IN(
+Select 
+TRIM MAX(Prod_price) 
+From PRODUCTS
+ );
 -- BY Smoliak_Yurii
