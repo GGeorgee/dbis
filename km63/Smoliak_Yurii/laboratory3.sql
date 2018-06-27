e413bbfb2f9760817e4e2a9a7a15351125647447
GGeorgee <37927842+GGeorgee@users.noreply.github.com>
2018-06-27 15:55:26 +0300

    Update laboratory3.sql

--- km63/Smoliak_Yurii/laboratory3.sql
+++ km63/Smoliak_Yurii/laboratory3.sql
@@ -1,2 +1,28 @@
 -- LABORATORY WORK 3
+/*2*/
+Select
+TRIM VEND_ID
+TRIM VEND_NAME
+From Vendors
+
+
+
+/*3.1*/
+Select
+TRIM Order_NUM
+TRIM ORDER_ITEM
+From Orderitems
+
+
+/*3.2*/
+Select
+TRIM Vend_name
+From Vendors
+Where in (Select (Order_Num) 
+from ORDERS )
+
+/*1*/
+Select (Cust_name , Cust_Id )
+From Customers
+Where IN (Select (Order_Date , Prod_ID )
 -- BY Smoliak_Yurii
